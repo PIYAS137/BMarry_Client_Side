3507ed0207f1b67719572faa4074d09cc40b6e84
S M Piyas Mahamude Alif <piyasmahmudealif@gmail.com>
2023-11-25 15:14:18 +0600

    add login and register page

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,8 +1,24 @@
+import { Link } from "react-router-dom"
 
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className=" h-screen flex justify-center items-center">
+      <form action="" className=" p-7 bg-red-200 max-w-2xl mx-auto -mt-32 rounded-xl">
+        <h1 className="text-4xl text-center font-bold mb-5">Log In User</h1>
+        <label>Enter your email</label>
+        <input type="email" name='name' placeholder="enter your email" className="mt-1 w-full mb-3 px-3 py-2 rounded-lg text-lg" />
+        <label>Enter your passsword</label>
+        <input type="email" name='name' placeholder="enter your email" className="mt-1 w-full px-3 py-2 rounded-lg text-lg" />
+        <p className=" mt-1">Dont have an account ? Go to <Link className=" font font-semibold text-pink-500" to={'/registration'}>Register Page</Link></p>
+        <div className=" flex justify-center">
+          <button className="w-full bg-pink-500 text-white px-5 py-2 mt-3 rounded-lg text-lg">LOG IN</button>
+        </div>
+        <div className=" flex justify-center">
+          <button className="w-full bg-purple-500 text-white px-5 py-2 mt-3 rounded-lg text-lg">LOG IN WITH GOOGLE</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
--- src/Pages/RegistrationPage/RegistrationPage.jsx
+++ src/Pages/RegistrationPage/RegistrationPage.jsx
@@ -1,8 +1,29 @@
+import { Link } from "react-router-dom"
 
 
 const RegistrationPage = () => {
   return (
-    <div>RegistrationPage</div>
+    <div className=" h-screen flex justify-center items-center">
+      <form action="" className=" p-7 bg-purple-200 max-w-2xl mx-auto -mt-32 rounded-xl">
+        <h1 className="text-4xl text-center font-bold mb-5">Register User</h1>
+        <label>Enter your name</label>
+        <input type="text" name='name' placeholder="enter your name" className="mt-1 w-full mb-3 px-3 py-2 rounded-lg text-lg" />
+        
+        <label>Enter your phoro URL</label>
+        <input type="text" name='photo' placeholder="enter your phoro URL" className="mt-1 w-full mb-3 px-3 py-2 rounded-lg text-lg" />
+        
+        <label>Enter your email</label>
+        <input type="email" name='email' placeholder="enter your email" className="mt-1 w-full mb-3 px-3 py-2 rounded-lg text-lg" />
+        
+        <label>Enter your passsword</label>
+        <input type="password" name='password' placeholder="enter your password" className="mt-1 w-full px-3 py-2 rounded-lg text-lg" />
+        
+        <p className=" mt-1">Already have an account ? Go to <Link className=" font font-semibold text-pink-500" to={'/login'}>Login Page</Link></p>
+        <div className=" flex justify-center">
+          <button className="w-full bg-pink-500 text-white px-5 py-2 mt-3 rounded-lg text-lg">REGISTER</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
